3f928d16a48c484cdb7e60e10d424c791332f089
Anastasiia <soloventanni@gmail.com>
2025-03-11 19:02:36 +0100

    Add homework 6

---
+++ HomeWork_6/6task1.js
@@ -0,0 +1,15 @@
+function calculateArea1(width, height) { //function declaration
+    return width * height;
+    }
+
+console.log(calculateArea1(5, 10));
+    
+const calculateArea2 = function(width, height) { //function expression
+        return width * height;
+    }
+
+console.log(calculateArea2(5, 10));
+    
+const calculateArea3 = (width, height) => width * height;  //arrow function
+
+console.log(calculateArea3(5, 10));
